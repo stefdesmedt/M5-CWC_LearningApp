cbd5c81856aa3fcc6cacbbd23ccc5ef446b9304c
steffds <sdesmedt7@gmail.com>
2022-08-19 10:56:20 +0200

    Setup folder structure and create content model as an observable object

---
+++ LearningApp/ViewModels/ContentModel.swift
@@ -0,0 +1,14 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by Stephanie De Smedt on 19/08/2022.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+    
+    
+}
