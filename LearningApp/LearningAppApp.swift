7e1e201b9c2af5078b730e3d14b3f1327c2c90d0
steffds <sdesmedt7@gmail.com>
2022-08-19 10:46:00 +0200

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Stephanie De Smedt on 19/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
